e889812aa6f94a4eb6f85d0c43cabe5f0fbef6c1
shihua.he <330967496@qq.com>
2017-04-28 18:32:47 +0800

    调整controller-func

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -5,7 +5,7 @@
 // 创建 application/x-www-form-urlencoded 编码解析
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-// 自定义组件
+// 自定义模块
 var connection = require('../bin/connection');
 connection.connect();
 var utils = require('../tools/utils');
@@ -17,18 +17,12 @@ module.exports = function(app){
 
     utils.get(app,reqPath('/getData'),function(req, res){
         connection.query('select * from tbl_test', function (error, results, fields) {
-            // if (error) throw error;
-            error?console.log(error):'';
-            console.log(results);
             resData(error,results,res);
         });
     });
 
     utils.post(app,reqPath('/postData'),function(req, res){
         connection.query('select * from tbl_test', function (error, results, fields) {
-            // if (error) throw error;
-            error?console.log(error):'';
-            console.log(results);
             resData(error,results,res);
         });
     });
@@ -41,6 +35,10 @@ var reqPath = function(url){
 
 // 响应数据
 var resData = function(error,results,res){
+    // if (error) throw error;
+    error?console.log(error):'';
+    console.log(results);
+
     var data = {
         code:0,
         data:{
